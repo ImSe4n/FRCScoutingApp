c1b9f58097a3917ac99364f5c5b3243fb7a91433
Sean Nie <snie1@ocdsb.ca>
2026-06-01 10:27:12 -0400

    start coding match

---
+++ Match.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Match here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Match
+{
+    private Alliance blueAlliance;
+    private Alliance redAlliance;
+    private byte bytMatchNumber;
+
+    
+}
