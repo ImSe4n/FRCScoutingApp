f00d736a031fa308cee6ef1a763b812b9f9cc204
Sean Nie <81334381+ImSe4n@users.noreply.github.com>
2026-06-06 14:23:12 -0400

    add

--- frc-scout-pro/mobile/api/client.ts
+++ frc-scout-pro/mobile/api/client.ts
@@ -12,7 +12,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(async (config) => {
-  const token = await AsyncStorage.getItem('to  ken');
+  const token = await AsyncStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
